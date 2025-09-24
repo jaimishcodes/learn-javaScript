36e61e19bcaf4f3a14302ee2975893f7b57c3b31
jaimishcodes <jaimishkachariya2007@gmail.com>
2025-09-24 14:29:38 +0000

    Conversion And Operation

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,25 @@
+let score = "Jaimish";
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+
+//"20"=> 20
+//"20abc"=>NaN
+//"true"=> 1 
+//"False"=>0
+//"undefine"=>NaN
+//"null"=>Nan
+
+// change number to boolean
+let isLoggedIn = 0;
+let BooleanisLoggedIn = Boolean(isLoggedIn);
+console.log(BooleanisLoggedIn);
+
+//change number string
+let name = 420;
+let stringName = String(name);
+console.log(typeof(stringName));
