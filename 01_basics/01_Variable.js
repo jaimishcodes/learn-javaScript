672ddf8f888601252f7b9f9ab50024ecff467428
jaimishcodes <jaimishkachariya2007@gmail.com>
2025-09-24 13:53:53 +0000

    DataType

---
+++ 01_basics/01_Variable.js
@@ -0,0 +1,11 @@
+const accountId = 120;
+let accountEmail = "jaimish@2007";
+var accountPassword ="123";
+accountCity = "Mumbai";
+
+// accountid = 2;
+accountEmail = "Lalalajptray";
+accountCity = "Gujarat"; 
+
+console.log(accountId);
+console.table([accountEmail,accountCity])
---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,13 @@
+// alert("Hello"); // We are Use Nodejs, not Browser
+
+//Code readability is Importante
+
+//datatype
+
+let name = "jaimish";// String
+let age = 20;//Number
+let isLoggedIn = true;//Boolean
+let Number;//undefine
+let id = null; // null
+
+
